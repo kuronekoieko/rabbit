ccb9a9fbb266054db02ae7161e3dd3a8571ab6ab
ieko <mendo931411.mou@gmail.com>
2018-12-11 23:19:51 +0900

    空行削除

--- Assets/script/beeController.cs
+++ Assets/script/beeController.cs
@@ -7,14 +7,11 @@ public class beeController : MonoBehaviour {
     Rigidbody2D rigid2D;
     Animator animator;
 
-
-
     void Start () {
         rigid2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         rigid2D.gravityScale = 0;
     }
-	
 	
 	void Update () {
         rigid2D.velocity = new Vector3(0,0,0);
